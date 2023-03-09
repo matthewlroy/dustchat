55537a0c4c76b62d097cde7b4536415fd9f6602b
Matthew Roy <matthew@saplink.io>
2023-03-09 19:21:29 +0000

    update create account form with accessibility, update script to perform health check and get new api file

--- res/js/script.js
+++ res/js/script.js
@@ -3,73 +3,35 @@ const DEFAULT_FETCH_TIMEOUT = {
   MILLISECONDS: 8000,
   SECONDS: 8,
 };
-
-const ENDPOINTS_FILE = `/endpoints.json`;
 //#endregion
 
 //#region GLOBAL HELPERS
-let api_endpoints = {};
+let API_ENDPOINTS = {};
 //#endregion
 
 //#region MAIN
 window.addEventListener(`DOMContentLoaded`, async (event) => {
   const start = performance.now();
 
-  // // Load in the endpoints required API endpoints for the application
-  // api_endpoints = await get_api_endpoints_json()
-  //   .then((jsonResponse) => jsonResponse)
-  //   .catch(() => { return {}; });
+  // Load in the endpoints required API endpoints for the application
+  API_ENDPOINTS = await get_api_endpoints_json()
+    .then((jsonResponse) => jsonResponse)
+    .catch(() => { return {}; });
 
-  // // Check we properly loaded in those endpoints
-  // if (api_endpoints && Object.keys(api_endpoints).length === 0 &&
-  //   Object.getPrototypeOf(api_endpoints) === Object.prototype) {
-  //   show_error_in_body(`Unable to obtain API endpoints.`);
-  //   return;
-  // }
+  // Check we properly loaded in those endpoints
+  if (API_ENDPOINTS && Object.keys(API_ENDPOINTS).length === 0 &&
+    Object.getPrototypeOf(API_ENDPOINTS) === Object.prototype) {
+    const error = `Unable to obtain API endpoints.`;
+    show_error_in_body(error);
+    throw new Error(error);
+  }
 
-  // // Health check against our server prior to any further ops
-  // if (!await get_server_online_status()) {
-  //   show_error_in_body(`Could not connect to server.`);
-  //   return;
-  // }
-
-  // // Set necessary event listeners
-  // // document.getElementById(`create_account_action_btn`)
-  // //   .addEventListener(`click`, (e) => {
-  // //     e.preventDefault();
-  // //     document.getElementById(`create_account_form_container`).hidden = false;
-  // //   });
-
-  // const create_account_form = document.getElementById(`create_account_form`);
-  // create_account_form.addEventListener(`submit`, async (e) => {
-  //   e.preventDefault();
-
-  //   const create_account_form_data = new FormData(e.target);
-  //   const formDataArray = [...create_account_form_data];
-  //   const formData = Object.fromEntries(formDataArray);
-  //   console.log(`frontend: ${JSON.stringify(formData)}`);
-
-  //   // TODO: Validation
-
-  //   try {
-  //     const response = await fetch_wrapper(
-  //       api_endpoints.CREATE_ACCOUNT,
-  //       {
-  //         method: `POST`,
-  //         mode: `same-origin`,
-  //         cache: `reload`,
-  //         credentials: `same-origin`,
-  //         referrerPolicy: `no-referrer`,
-  //         headers: {
-  //           "Content-Type": `application/json`,
-  //         },
-  //         body: JSON.stringify(formData),
-  //       },
-  //     );
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // });
+  // Health check against our server prior to any further ops
+  if (!await get_server_online_status()) {
+    const error = `Could not connect to server.`;
+    show_error_in_body(error);
+    throw new Error(error);
+  }
 
   // Hide loaders and show content
   document.getElementById(`loading_overlay`).hidden = true;
@@ -78,6 +40,50 @@ window.addEventListener(`DOMContentLoaded`, async (event) => {
   const end = performance.now();
   console.log(`[script.js] >> ${event.type} <<\n\tExecution time: ${Math.round(end - start)}ms`);
 });
+//#endregion
+
+//#region API CALLS
+async function get_server_online_status() {
+  try {
+    const response = await fetch_wrapper(
+      API_ENDPOINTS.health_check,
+      {
+        method: `GET`,
+        mode: `same-origin`,
+        cache: `reload`,
+        credentials: `same-origin`,
+        referrerPolicy: `no-referrer`,
+      },
+    );
+
+    return response.ok;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+async function get_api_endpoints_json() {
+  const ENDPOINTS_FILE_NAME = `/endpoints_v1.json`;
+
+  try {
+    const response = await fetch_wrapper(
+      ENDPOINTS_FILE_NAME,
+      {
+        method: `GET`,
+        mode: `same-origin`,
+        cache: `reload`,
+        credentials: `same-origin`,
+        referrerPolicy: `no-referrer`,
+      },
+    );
+
+    return response.json();
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
+}
 //#endregion
 
 //#region HELPER FUNCTIONS
@@ -100,48 +106,10 @@ async function fetch_wrapper(resource, options = {}) {
   return response;
 }
 
-async function get_server_online_status() {
-  try {
-    const response = await fetch_wrapper(
-      api_endpoints.HEALTH_CHECK,
-      {
-        method: `GET`,
-        mode: `same-origin`,
-        cache: `reload`,
-        credentials: `same-origin`,
-        referrerPolicy: `no-referrer`,
-      },
-    );
-
-    return response.ok;
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
-}
-
-async function get_api_endpoints_json() {
-  try {
-    const response = await fetch_wrapper(
-      ENDPOINTS_FILE,
-      {
-        method: `GET`,
-        mode: `same-origin`,
-        cache: `reload`,
-        credentials: `same-origin`,
-        referrerPolicy: `no-referrer`,
-      },
-    );
-
-    return response.json();
-  } catch (error) {
-    console.error(error);
-    return {};
-  }
-}
-
 async function show_error_in_body(errorMsg) {
-  console.error(errorMsg);
-  document.body.innerHTML = errorMsg;
+  document.body.innerHTML = `
+    <p>Something went wrong, please try again later.</p>
+    <p>Error: ${errorMsg ? `[${errorMsg}]` : "[]"}</p>
+  `;
 }
 //#endregion
